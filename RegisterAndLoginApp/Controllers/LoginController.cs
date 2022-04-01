4c58799d9d0645d4836c2f88e42e70aca9fea812
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2022-03-31 21:48:15 -0700

    init
    CST-350 Activity 2: Create a Login Page.
    
    Use Routes, Controllers, Models, and Views to build a simple Login Page
    using a N-Layer Web Application Architecture.

---
+++ RegisterAndLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLoginApp.Models;
+using RegisterAndLoginApp.Services;
+
+namespace RegisterAndLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel user)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if (securityService.isValid(user))
+            {
+                return View("LoginSuccess", user);
+            }
+            else
+            {
+                return View("LoginFailed", user);
+            }
+        }
+    }
+}
