2f411a287da6307e98a0e2491920a17818041d54
ricardo-monreal <moises.monreal@gmail.com>
2022-04-02 13:56:14 -0700

    Part 3 complete

---
+++ AppointmentMaker/Controllers/AppointmentsController.cs
@@ -0,0 +1,20 @@
+﻿using AppointmentMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentMaker.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAppointmentDetails(AppointmentModel appointmentModel)
+        {
+            return View("ShowAppointmentDetails", appointmentModel);
+        }
+    }
+
+
+}
--- AppointmentMaker/Models/AppointmentModel.cs
+++ AppointmentMaker/Models/AppointmentModel.cs
@@ -1,6 +1,50 @@
-﻿namespace AppointmentMaker.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentMaker.Models
 {
     public class AppointmentModel
     {
+        
+
+        [Required]
+        [DisplayName("Patient's Full Name")]
+        [StringLength(20, MinimumLength = 4)]
+        public String patientName { get; set; }
+
+        [Required]
+        [DisplayName("Appointment Request Date")]
+        [DataType(DataType.Date)]
+        public DateTime dateTime { get; set; }
+
+        [Required]
+        [DisplayName("Patient's approximate net worth")]
+        [DataType(DataType.Currency)]
+        public decimal PatientNetWorth { get; set; }
+
+        [Required]
+        [DisplayName("Primary Doctor's Last Name")]
+        public string DoctorName { get; set; }
+
+        [Required]
+        [DisplayName("Patient's perceived level of pain (1 low to 10 high")]
+        [Range(1, 10)]
+        public int PainLevel { get; set; }
+
+
+        public AppointmentModel(string patientName, DateTime dateTime, decimal patientNetWorth, string doctorName, int painLevel)
+        {
+            this.patientName = patientName;
+            this.dateTime = dateTime;
+            PatientNetWorth = patientNetWorth;
+            DoctorName = doctorName;
+            PainLevel = painLevel;
+        }
+
+        public AppointmentModel()
+        {
+
+        }
+
     }
 }
