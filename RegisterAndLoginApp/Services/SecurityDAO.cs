184f3cd536ba673674e299e544cd5990bb23417e
ricardo-monreal <54251540+ricardo-monreal@users.noreply.github.com>
2022-04-02 12:32:29 -0700

    Part 2 Complete

---
+++ RegisterAndLoginApp/Services/SecurityDAO.cs
@@ -0,0 +1,45 @@
+﻿using RegisterAndLoginApp.Models;
+using System.Data.SqlClient;
+
+namespace RegisterAndLoginApp.Services
+{
+    public class SecurityDAO
+    {
+        string connectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+
+        public bool FindUserByNameAndPassword(UserModel user)
+        {
+            bool success = false;
+
+            string sqlStatement = "SELECT * FROM dbo.users WHERE USERNAME = @UserName AND PASSWORD = @Password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                command.Parameters.Add("@UserName", System.Data.SqlDbType.VarChar, 50).Value = user.UserName;
+                command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                        success = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                };
+            }
+
+            return success;
+        }
+
+    }
+
+   
+}
--- RegisterAndLoginApp/Services/SecurityService.cs
+++ RegisterAndLoginApp/Services/SecurityService.cs
@@ -6,21 +6,26 @@ namespace RegisterAndLoginApp.Services
 
     public class SecurityService
     {
-        List<UserModel> knownUsers = new List<UserModel>();
+
+        SecurityDAO securityDAO = new SecurityDAO();
 
 
-        public SecurityService()
-        {
-            knownUsers.Add(new UserModel() { Id = 0, UserName = "Michael", Password = "Scott" });
-            knownUsers.Add(new UserModel() { Id = 1, UserName = "Dwight", Password = "Schrute" });
-            knownUsers.Add(new UserModel() { Id = 2, UserName = "Pamela", Password = "Beesly" });
-            knownUsers.Add(new UserModel() { Id = 3, UserName = "Kevin", Password = "Malone" });
-            knownUsers.Add(new UserModel() { Id = 4, UserName = "Angela", Password = "Martin" });
-        }
+
+        //List<UserModel> knownUsers = new List<UserModel>();
+
+
+        //public SecurityService()
+        //{
+        //    knownUsers.Add(new UserModel() { Id = 0, UserName = "Michael", Password = "Scott" });
+        //    knownUsers.Add(new UserModel() { Id = 1, UserName = "Dwight", Password = "Schrute" });
+        //    knownUsers.Add(new UserModel() { Id = 2, UserName = "Pamela", Password = "Beesly" });
+        //    knownUsers.Add(new UserModel() { Id = 3, UserName = "Kevin", Password = "Malone" });
+        //    knownUsers.Add(new UserModel() { Id = 4, UserName = "Angela", Password = "Martin" });
+        //}
 
         public bool isValid(UserModel user)
         {
-            return knownUsers.Any(x => x.UserName == user.UserName && x.Password == user.Password);
+            return securityDAO.FindUserByNameAndPassword(user);
         }
     }
 }
