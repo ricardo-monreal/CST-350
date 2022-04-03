bfc82b1f7548558e04f2918e072792324f13a770
ricardo-monreal <moises.monreal@gmail.com>
2022-04-03 10:40:00 -0700

    Activity 1

---
+++ ASPCoreFirstApp/ASPCoreFirstApp/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace ASPCoreFirstApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            ViewBag.name = "Ricardo";
+            ViewBag.secretNumber = 13;
+            return View();
+        }
+    }
+}
