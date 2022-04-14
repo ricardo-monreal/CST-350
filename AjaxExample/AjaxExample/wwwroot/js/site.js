747977a6c62cf727b26441432782c80ba378687c
ricardo-monreal <moises.monreal@gmail.com>
2022-04-14 16:29:17 -0700

    Activity 4 part 1 and 2 complete

---
+++ AjaxExample/AjaxExample/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    console.log("Page is ready");
+
+    $(".customerRadio").change(function () {
+        console.log("Radio button selected");
+        doCustomerUpdate()
+    });
+
+    $("#selectCustomer").click(function (event) {
+        event.preventDefault();
+        console.log("Submit button was clicked");
+        doCustomerUpdate();
+
+    });
+
+    function doCustomerUpdate() {
+
+        $.ajax({
+            type: "POST",
+            url: 'customer/ShowOnePerson',
+            data: $("form").serialize(),
+            success: function (data) {
+                console.log(data);
+                $("#customerInformationArea").html(data);
+            }
+        });
+    };
+});
